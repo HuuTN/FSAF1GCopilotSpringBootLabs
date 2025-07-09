1068e6156fb24a2cecc678fd1956f851accf4929
phamthanhtu99 <thanhtutp91@gmail.com>
2025-07-09 19:44:08 +0700

    Add Lab exercise 3

---
+++ springboot-lab/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Order> orders;
+}
